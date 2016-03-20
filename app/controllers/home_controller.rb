c7d34ed840df6cb6295fde26374970b36d8e44b6
Slava Pasechnik <slavapas13@gmail.com>
2016-03-20 16:57:23 +0000

    Init BB Questions

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,13 @@
+class HomeController < ApplicationController
+  def default
+  end
+
+  def about
+  end
+
+  def update
+  end
+
+  def contact_us
+  end
+end
---
+++ spec/controllers/home_controller_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe HomeController, type: :controller do
+
+  describe "GET #default" do
+    it "returns http success" do
+      get :default
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #about" do
+    it "returns http success" do
+      get :about
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #update" do
+    it "returns http success" do
+      get :update
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #contact_us" do
+    it "returns http success" do
+      get :contact_us
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
