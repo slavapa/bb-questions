a933f5fc239e3caf290c59406c322638f6af5d7b
Slava Pasechnik <slavapas13@gmail.com>
2016-04-14 13:24:03 +0000

    Add buttons Edit/Save/Cancel and tranlsation textarea for editing

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,8 +1,12 @@
 class Question < ActiveRecord::Base
-    after_create :send_notify
-    
-    validates :name, presence: true, length: { maximum: 255 }
-    validates :question, presence: true
+  before_validation(on: :create) do
+    translate
+  end
+  
+  after_create :send_notify
+  
+  validates :name, presence: true, length: { maximum: 255 }
+  validates :question, presence: true
     # validates :ip, presence: true
     
     
@@ -11,5 +15,9 @@ class Question < ActiveRecord::Base
     # Escape single quoted strings by inserting 2 of them
     self.class.connection.execute %Q(NOTIFY #{AsyncEvents::CHANNEL}, '#{to_json.gsub("'", "''")}')
     true
+  end
+  
+  def translate
+    self.translation = question if attribute_present?("question")
   end
 end
